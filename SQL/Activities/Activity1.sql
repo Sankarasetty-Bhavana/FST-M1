97402760faa549a5d0d700e7d30441e8d55b0d84
Sankarasetty-Bhavana <102509199+Sankarasetty-Bhavana@users.noreply.github.com>
2022-04-07 14:02:15 +0530

    Add files via upload

---
+++ SQL/Activities/Activity1.sql
@@ -0,0 +1,10 @@
+REM   Script: Activty1
+REM   fst-b26-m1
+
+CREATE TABLE salesman ( 
+    salesman_id int, 
+    salesman_name varchar2(32), 
+    salesman_city varchar2(32), 
+    commission int 
+);
+
